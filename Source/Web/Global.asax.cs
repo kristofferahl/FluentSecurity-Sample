f0d11e7b4baf39ae54e0d492bb7fec7b035cbbf5
Kristoffer Ahl <git@77dynamite.com>
2014-05-16 10:32:08 +0200

    - Added the full configuration needed to secure all public controller actions. - Configured FluentSecurity to use roles.

--- Source/Web/Global.asax.cs
+++ Source/Web/Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 using FluentSecurity;
 using FluentSecurity.Configuration;
 using Web.App_Start;
 using Web.Controllers;
+using Web.Models;
 
 namespace Web
 {
@@ -24,11 +27,25 @@ namespace Web
 				// Let FluentSecurity know how to get the authentication status of the current user
 				configuration.GetAuthenticationStatusFrom(() => HttpContext.Current.User.Identity.IsAuthenticated);
 
-				// This is where you set up the policies you want FluentSecurity to enforce on your controllers and actions
+				// Let FluentSecurity know how to get the roles for the current user
+				configuration.GetRolesFrom(() => Roles.GetRolesForUser(HttpContext.Current.User.Identity.Name));
+
+				// This is where you set up the policies you want Fluent Security to enforce on your controllers and actions
+				configuration.ForAllControllers().RequireAnyRole(UserRoles.Administrator);
+
 				configuration.For<HomeController>().Ignore();
+
 				configuration.For<AccountController>().DenyAuthenticatedAccess();
 				configuration.For<AccountController>(x => x.ChangePassword()).DenyAnonymousAccess();
+				configuration.For<AccountController>(x => x.ChangePasswordSuccess()).DenyAnonymousAccess();
 				configuration.For<AccountController>(x => x.LogOff()).DenyAnonymousAccess();
+
+				configuration.For<IssuesController>(x => x.Index()).DenyAnonymousAccess();
+				configuration.For<IssuesController>(x => x.Details(Guid.Empty)).DenyAnonymousAccess();
+				configuration.For<IssuesController>(x => x.Create()).RequireAnyRole(UserRoles.Member);
+				configuration.For<IssuesController>(x => x.Close(Guid.Empty)).RequireAnyRole(UserRoles.Employee, UserRoles.Administrator);
+
+				configuration.For<UsersController>().RequireAnyRole(UserRoles.Administrator);
 			});
 
 			GlobalFilters.Filters.Add(new HandleSecurityAttribute(), -1);
