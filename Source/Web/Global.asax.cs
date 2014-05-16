37ed6836d4531518f02747091bf575a147ac8933
Kristoffer Ahl <git@77dynamite.com>
2014-05-16 08:42:59 +0200

    - Addded the example security configuration for the Web project.

--- Source/Web/Global.asax.cs
+++ Source/Web/Global.asax.cs
@@ -1,7 +1,10 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using FluentSecurity;
+using FluentSecurity.Configuration;
 using Web.App_Start;
+using Web.Controllers;
 
 namespace Web
 {
@@ -15,6 +18,20 @@ namespace Web
 
 			FilterConfig.RegisterFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			SecurityConfigurator.Configure<MvcConfiguration>(configuration =>
+			{
+				// Let FluentSecurity know how to get the authentication status of the current user
+				configuration.GetAuthenticationStatusFrom(() => HttpContext.Current.User.Identity.IsAuthenticated);
+
+				// This is where you set up the policies you want FluentSecurity to enforce on your controllers and actions
+				configuration.For<HomeController>().Ignore();
+				configuration.For<AccountController>().DenyAuthenticatedAccess();
+				configuration.For<AccountController>(x => x.ChangePassword()).DenyAnonymousAccess();
+				configuration.For<AccountController>(x => x.LogOff()).DenyAnonymousAccess();
+			});
+
+			GlobalFilters.Filters.Add(new HandleSecurityAttribute(), -1);
 		}
 	}
 }
