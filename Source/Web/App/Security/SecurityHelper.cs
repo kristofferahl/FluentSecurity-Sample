3fdc2c889ce61f9b7351449b410e9354712406ff
Kristoffer Ahl <git@77dynamite.com>
2014-05-19 08:06:28 +0200

    - Implemented SecurityHelper.ActionIsAllowed to remove restricted links and buttons from the UI.

--- Source/Web/App/Security/SecurityHelper.cs
+++ Source/Web/App/Security/SecurityHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
+using FluentSecurity;
+using FluentSecurity.Configuration;
 
 namespace Web.App.Security
 {
@@ -8,6 +11,19 @@ namespace Web.App.Security
 	{
 		public static bool ActionIsAllowed<TController>(Expression<Func<TController, ActionResult>> actionExpression) where TController : IController
 		{
+			var actionName = new MvcActionNameResolver().Resolve(actionExpression);
+			var fullControllerName = new MvcControllerNameResolver().Resolve(typeof(TController));
+
+			var securityConfiguration = SecurityConfiguration.Get<MvcConfiguration>();
+			var policyContainer = securityConfiguration.Runtime.PolicyContainers.GetContainerFor(fullControllerName, actionName);
+			if (policyContainer != null)
+			{
+				var context = securityConfiguration.CreateContext();
+				var results = policyContainer.EnforcePolicies(context);
+
+				return results.All(x => x.ViolationOccured == false);
+			}
+
 			return true;
 		}
 	}
